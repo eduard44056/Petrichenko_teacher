b98dcd606926bfdf72644be18c0cee14a51b02b2
eduard44056 <galikeev.eduard@yandex.ru>
2020-06-02 06:40:51 +0500

    Задание №1

---
+++ js/script2.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let timeDay = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money = prompt('Ваш бюджет на месяц?', '');
+let mandatoryExpenses = prompt(
+  'Введите обязательную статью расходов в этом месяце',
+  ''
+);
+let howMuch = prompt('Во сколько обойдется?', '');
+let monthDay = 30;
+
+let appData = {
+  money,
+  timeDay,
+  expenses: { mandatoryExpenses: howMuch },
+  optionalExpenses: '',
+  income: '',
+  savings: false,
+};
+console.log(mandatoryExpenses + ' : ' + howMuch);
+let budgetDay = money / monthDay;
+alert('Бюджет на 1 день составляет ' + budgetDay);
