ec25e0ec3f078a73d4880d356d3af516f6674220
eduard44056 <galikeev.eduard@yandex.ru>
2020-06-02 07:02:27 +0500

    changed script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-"use strict";
+'use strict';
 
-console.log(typeof ("q" + 5));
-let a = prompt("Сколько Вам лет?", "");
-console.log("Вам " + a + " лет");
+// console.log(typeof ("q" + 5));
+// let a = prompt("Сколько Вам лет?", "");
+// console.log("Вам " + a + " лет");
+
+// let y = 1;
+// let x = (y = 2);
+// alert(x);
+
+// let x = [] + 1 + 2;
+// alert(x);
+// alert('1'[0]);
+
+let x = 2 && 1 && null && 0 && undefined;
+console.log(x);
